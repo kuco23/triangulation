d246ddbd35c9494bf43ff836fffe59760e4389f4
kuco23 <nseverkar@gmail.com>
2022-03-15 16:54:08 +0100

    cmaps, unnecessary imports

--- triangulation_medium.py
+++ triangulation_medium.py
@@ -1,13 +1,7 @@
-from operator import truediv
-import math
-from itertools import product
 import numpy as np
-from scipy.linalg import lstsq, schur
-from scipy.spatial import Delaunay
+from scipy.spatial import Delaunay, ConvexHull
 import matplotlib.pyplot as plt
-import matplotlib.tri as mtri
 from matplotlib import cm
-from matplotlib.colors import LightSource
 from mpl_toolkits.mplot3d import Axes3D
 
 ax = plt.figure().gca(projection='3d')
@@ -22,70 +16,72 @@ def readFile(file):
 def scatter(ax, file):
     v = readFile(file)
     for p in v: ax.scatter(*p, color='black', s=10)
-
-def triangulate(ax, file):
+            
+def triangulateFace(ax, file, cmap=cm.magma):
     v = np.array(readFile(file))
     tri = Delaunay(v[:,:2])
     ax.plot_trisurf(
         v[:,0], v[:,1], v[:,2],
         triangles = tri.simplices,
-        cmap=cm.inferno
+        cmap=cmap
     )
 
-def drawSphere(ax, k=30):
+def triangulateSphere(ax, k=30, cmap=cm.magma):
   
     # sphere parametrization
-    T = np.linspace(0, 2 * np.pi, k)
-    X = np.zeros((k,k))
-    Y = np.zeros((k,k))
-    Z = np.zeros((k,k))
+    U = np.linspace(0, 2 * np.pi, k)
+    V = np.linspace(0, np.pi, k)
+    S1 = np.zeros((k,k))
+    S2 = np.zeros((k,k))
+    S3 = np.zeros((k,k))
     for i in range(k):
         for j in range(k):
-            X[i,j] = np.cos(T[i]) * np.sin(T[j])
-            Y[i,j] = np.sin(T[i]) * np.sin(T[j])
-            Z[i,j] = np.cos(T[j])
+            S1[i,j] = np.cos(U[i]) * np.sin(V[j])
+            S2[i,j] = np.sin(U[i]) * np.sin(V[j])
+            S3[i,j] = np.cos(V[j])
 
     # triangulate the points in [0,2pi] x [0,2pi]
-    [T1, T2] = np.meshgrid(T, T)
+    [T1, T2] = np.meshgrid(U, V)
     tri = Delaunay(np.array([T1.flatten(), T2.flatten()]).T)
     
     # plot the sphere
     ax.plot_trisurf(
-        X.flatten(), Y.flatten(), Z.flatten(),
-        triangles=tri.simplices, cmap=cm.inferno,
-        lightsource = LightSource()
+        S1.flatten(), S2.flatten(), S3.flatten(),
+        triangles=tri.simplices,
+        cmap=cmap
     )
 
-def triangulateEllipsoid(ax, A, k=30):
+def triangulateEllipsoid(ax, A, k=30,cmap=cm.magma):
+    
     # sphere parametrization
-    T = np.linspace(0, 2 * np.pi, k)
-    X = np.zeros((k,k))
-    Y = np.zeros((k,k))
-    Z = np.zeros((k,k))
+    U = np.linspace(0, 2 * np.pi, k)
+    V = np.linspace(0, np.pi, k)
+    S1 = np.zeros((k,k))
+    S2 = np.zeros((k,k))
+    S3 = np.zeros((k,k))
     for i in range(k):
         for j in range(k):
-            X[i,j] = np.cos(T[i]) * np.sin(T[j])
-            Y[i,j] = np.sin(T[i]) * np.sin(T[j])
-            Z[i,j] = np.cos(T[j])
+            S1[i,j] = np.cos(U[i]) * np.sin(V[j])
+            S2[i,j] = np.sin(U[i]) * np.sin(V[j])
+            S3[i,j] = np.cos(V[j])
 
     # map sphere to elipsoid
-    U1 = np.zeros((k,k))
-    U2 = np.zeros((k,k))
-    U3 = np.zeros((k,k))
+    E1 = np.zeros((k,k))
+    E2 = np.zeros((k,k))
+    E3 = np.zeros((k,k))
     for i in range(k):
         for j in range(k):
-            xyz = np.array([X[i,j], Y[i,j], Z[i,j]])
-            [U1[i,j], U2[i,j], U3[i,j]] = A @ xyz
+            xyz = np.array([S1[i,j], S2[i,j], S3[i,j]])
+            [E1[i,j], E2[i,j], E3[i,j]] = A @ xyz
 
     # triangulate the elipsoid
-    [T1, T2] = np.meshgrid(T, T)
+    [T1, T2] = np.meshgrid(U, V)
     tri = Delaunay(np.array([T1.flatten(), T2.flatten()]).T)
     
     # plot the elipsoid
     ax.plot_trisurf(
-        U1.flatten(), U2.flatten(), U3.flatten(),
-        triangles=tri.simplices, cmap=cm.inferno,
-        lightsource = LightSource()
+        E1.flatten(), E2.flatten(), E3.flatten(),
+        triangles=tri.simplices, cmap=cmap
     )
 
 
@@ -97,5 +93,4 @@ A = np.array([
 
 triangulateEllipsoid(ax, A)
 plt.show()
-        
-    
+
